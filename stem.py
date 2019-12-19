df96100baa576104ddfd846b3d82f19e47482a17
taha ashtiani <ashtiani.phys@gmail.com>
2019-12-19 11:13:46 -0500

    Update stem.py

--- stem.py
+++ stem.py
@@ -1,10 +1,3 @@
-# find this part in torrc file in etc
-# and commentout ControlPort so that stem can use it. for controlling tor
-## The port on which Tor will listen for local connections from Tor
-## controller applications, as documented in control-spec.txt.
-#ControlPort 9051
-
-
 from stem import Signal
 from stem.control import Controller
 
